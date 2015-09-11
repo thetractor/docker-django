36b01e90bae483dd2b9d58c0f827b5b0c4df8699
Dirk Moors <dirkmoors@gmail.com>
2015-09-11 10:02:14 +0200

    Added mechanism to supply build dependencies while running tox

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -30,6 +30,36 @@ help    : Show this message
 """
 }
 
+# Install build dependencies
+install_build_deps(){
+    if [ -f ${DEPLOYMENTDIR}/builddeps.txt ];
+    then
+        echo "Installing build dependencies..."
+        set -e \
+            && buildDeps=`cat ${DEPLOYMENTDIR}/${BUILDDEPSFILE}` \
+            && echo ${buildDeps} \
+            && apt-get update \
+            && apt-get install -y ${buildDeps}
+    fi
+}
+
+# Uninstall build dependencies
+uninstall_build_deps(){
+    if [ -f ${DEPLOYMENTDIR}/builddeps.txt ];
+    then
+        echo "Uninstalling build dependencies..."
+        set -e \
+            && buildDeps=`cat ${DEPLOYMENTDIR}/${BUILDDEPSFILE}` \
+            && echo ${buildDeps} \
+            && rm -rf /var/lib/apt/lists/* \
+            && find /usr/local \
+                \( -type d -a -name test -o -name tests \) \
+                -o \( -type f -a -name '*.pyc' -o -name '*.pyo' \) \
+                -exec rm -rf '{}' + \
+            && apt-get purge -y --auto-remove ${buildDeps}
+    fi
+}
+
 # Define manage.py name
 if [ -z $MANAGEFILE ]; then
     export MANAGEFILE=manage.py
@@ -58,9 +88,17 @@ case "$1" in
         python ${APPDIR}/${MANAGEFILE} test
     ;;
     tox)
+        trap uninstall_build_deps EXIT INT TERM
+
+        install_build_deps
+
         echo "Running Tox Tests..."
         cd ${TOXFILEDIR} \
             && tox "${@:2}"
+
+        trap - EXIT
+        uninstall_build_deps
+        exit 0
     ;;
     shell)
         python ${APPDIR}/${MANAGEFILE} shell
