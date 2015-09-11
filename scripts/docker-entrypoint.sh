80b857bffc8f4590f5b4d58342a33d1ec8529618
Dirk Moors <dirkmoors@gmail.com>
2015-09-11 12:04:33 +0200

    Split up base python image

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-# Include configuration bootstrap scripts
-source "${SCRIPTSDIR}/make_configurations.sh"
+# Include
+source "${SCRIPTSDIR}/docker-python-commands.sh"
+source "${SCRIPTSDIR}/docker-django-commands.sh"
 
 # Define help message
 show_help() {
@@ -30,36 +31,6 @@ help    : Show this message
 """
 }
 
-# Install build dependencies
-install_build_deps(){
-    if [ -f ${DEPLOYMENTDIR}/builddeps.txt ];
-    then
-        echo "Installing build dependencies..."
-        set -e \
-            && buildDeps=`cat ${DEPLOYMENTDIR}/${BUILDDEPSFILE}` \
-            && echo ${buildDeps} \
-            && apt-get update \
-            && apt-get install -y ${buildDeps}
-    fi
-}
-
-# Uninstall build dependencies
-uninstall_build_deps(){
-    if [ -f ${DEPLOYMENTDIR}/builddeps.txt ];
-    then
-        echo "Uninstalling build dependencies..."
-        set -e \
-            && buildDeps=`cat ${DEPLOYMENTDIR}/${BUILDDEPSFILE}` \
-            && echo ${buildDeps} \
-            && rm -rf /var/lib/apt/lists/* \
-            && find /usr/local \
-                \( -type d -a -name test -o -name tests \) \
-                -o \( -type f -a -name '*.pyc' -o -name '*.pyo' \) \
-                -exec rm -rf '{}' + \
-            && apt-get purge -y --auto-remove ${buildDeps}
-    fi
-}
-
 # Define manage.py name
 if [ -z $MANAGEFILE ]; then
     export MANAGEFILE=manage.py
@@ -68,52 +39,31 @@ fi
 # Run
 case "$1" in
     dev)
-        echo "Running Development Server..."
-        export DJDEBUG=1
-        python ${APPDIR}/${MANAGEFILE} runserver 0.0.0.0:${PORT}
+        run_dev
     ;;
     worker)
-        echo "Running Worker (Celery)..."
-        celery worker \
-            --uid=www-data \
-            --gid=www-data \
-            --app=${CELERYAPP} \
-            --loglevel=${CELERYWORKER_LOGLEVEL}
+        run_worker
     ;;
     bash)
-        /bin/bash "${@:2}"
+        run_bash
     ;;
     test)
-        echo "Running Django Tests..."
-        python ${APPDIR}/${MANAGEFILE} test
+        run_django_tests
     ;;
     tox)
-        trap uninstall_build_deps EXIT INT TERM
-
-        install_build_deps
-
-        echo "Running Tox Tests..."
-        cd ${TOXFILEDIR} \
-            && tox "${@:2}"
-
-        trap - EXIT
-        uninstall_build_deps
-        exit 0
+        run_tox
     ;;
     shell)
-        python ${APPDIR}/${MANAGEFILE} shell
+        run_django_shell
+    ;;
+    uwsgi)
+        run_uwsgi
+    ;;
+    python)
+        run_python
     ;;
     help)
         show_help
-    ;;
-    uwsgi)
-        echo "Running App (uWSGI)..."
-        make_uwsgi_config
-        uwsgi \
-            --ini ${RUNDIR}/uwsgi.ini
-    ;;
-    python)
-        ptipython "${@:2}"
     ;;
     *)
         show_help
