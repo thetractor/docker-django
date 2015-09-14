edc0c71636dd68784a0e5107d12ea3dc278d9888
Dirk Moors <dirkmoors@gmail.com>
2015-09-14 10:47:26 +0200

    Removed APPDIR, introduced DJANGOROOT

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -12,18 +12,15 @@ Usage: docker run <imagename> COMMAND
 
 Commands
 
-dev      : Start a normal Django development server. Provide the 'APPDIR' env
-           which specifies the name of the dir that contains the manage.py file.
-           Provide 'MANAGEFILE' to be the name of the manage.py file.
-           Optionally provide the 'PORT' env
+dev      : Start a normal Django development server. Optionally provide the 'PORT' env
            variable to determine the port on which it listens.
 worker   : Start a celery worker. Requires the env variables 'CELERYAPP' and
            'CELERYWORKER_LOGLEVEL'
 bash     : Start a bash shell
-test     : Run the Django tests. Requires 'APPDIR' and 'MANAGEFILE'
+test     : Run the Django tests. Optionally provide custom 'MANAGEFILE'.
 tox      : Run the Tox tests. Requires 'TOXFILEDIR' and tox to be installed
-shell    : Start a Django Python shell. Requires 'APPDIR' and 'MANAGEFILE'
-uwsgi    : Run uwsgi server. Requires the env variables 'APPDIR', 'WSGIFILE',
+shell    : Start a Django Python shell. Optionally provide custom 'MANAGEFILE'.
+uwsgi    : Run uwsgi server. Requires the env variables 'DJANGOROOT', 'WSGIFILE' and
            'DJANGO_SETTINGS_MODULE'. Optional env variables are 'PORT',
            'UWSGIPORT' and 'STATUSPORT'.
 python   : Run a classic python shell
